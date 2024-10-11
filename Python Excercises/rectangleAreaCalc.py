f75d15f10800f247f8eb88cb61de38543ed6950a
dawidzurawski <dawidzurawskidrz@gmail.com>
2024-10-11 21:05:36 +0100

    Fix variable name

--- Python Excercises/rectangleAreaCalc.py
+++ Python Excercises/rectangleAreaCalc.py
@@ -1,6 +1,6 @@
 length = int(input("Enter the length: "))
 width = int(input("Enter the width: "))
 
-rectangleWidth = length * width
+area = length * width
 
-print(f"The rectangles width is: {rectangleWidth}")
+print(f"The rectangles width is: {area}")
